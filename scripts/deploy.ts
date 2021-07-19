01016190f15ffae0bd626e37d4a694f2287af020
Holly Grimm <holly.grimm@gmail.com>
2021-07-19 16:29:30 -0600

    move console msg

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,11 +11,12 @@ let deployer: SignerWithAddress;
 async function main() {
   var PROXY_REGISTRATION_ADDRESS = '0xa5409ec958c83c3f309868babaca7c86dcb077c1';
   const network = await ethers.provider.getNetwork();
+  console.log(`network: ${network.name}`);
+  
   if (network.name === "rinkeby") {
     console.log('using opensea registration address for rinkeby');
     PROXY_REGISTRATION_ADDRESS = '0xf57b2c51ded3a29e6891aba85459d600256cf317';
   }
-  console.log(`network: ${network.name}`);
 
   [deployer] = await ethers.getSigners();
   const address = await deployer.getAddress();
